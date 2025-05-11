e0b5f6602e1b18a1ab87d9acae84e3450ddfc317
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-11 10:31:45 +0900

    文字列を表示

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -4,6 +4,15 @@ import { createRoot } from 'react-dom/client';
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
+const element = (
+  <div>
+    <h1>hello World!</h1>
+    <p>Reactを学習中!</p>
+  </div>
+)
+
+
+
 // Render your React component instead
 const root = createRoot(document.getElementById('app')!);
-root.render(<h1>Hello, world</h1>);
+root.render(element);
