b0352402f5e6342ff60cd887300c0a9f4d88e8d8
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-12 21:58:26 +0900

    useStateを使って、Todoを追加

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -4,10 +4,26 @@ import { Heading } from "./components/Heading";
 import { useState } from "react";
 import { TextField } from "./components/TextField";
 
+export type Todo = {
+  id: number;
+  task: string;
+  person: string;
+  deadline: string;
+};
+
 export const App = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
   const [newTask, setNewTask] = useState<string>("");
   const [newPerson, setNewPerson] = useState<string>("");
   const [newDeadline, setNewDeadline] = useState<string>("");
+
+  const addNewTodo = () => {
+    setTodoList((prev) => [
+      ...prev,
+      { id: Date.now(), task: newTask, person: newPerson, deadline: newDeadline },
+    ]);
+  };
+
   return (
     <main className="text-center mx-auto w-4/5">
       <Heading level="h1">TODO</Heading>
@@ -22,17 +38,17 @@ export const App = () => {
         type="text"
         value={newPerson}
         onChange={setNewPerson}
-        />
+      />
       <TextField
         label="期限"
         type="date"
         value={newDeadline}
         onChange={setNewDeadline}
-        />
-      <p>{newTask}</p>
-      <p>{newPerson}</p>
-      <p>{newDeadline}</p>
-      <TodoList />
+      />
+      <button className="border" onClick={addNewTodo}>
+        追加
+      </button>
+      <TodoList todoList={todoList}/>
     </main>
   );
 };
--- src/js/components/TodoList.tsx
+++ src/js/components/TodoList.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { TodoItem } from "./TodoItem";
+import { Todo } from "../App";
 
-export const TodoList: React.FC = () => {
-  const todos = [
-    { id: 1, task: "souzi", person: "tanaka", deadline: "asitamade" },
-    { id: 2, task: "senntaku", person: "tanaka", deadline: "asattemade" },
-    { id: 3, task: "kaimono", person: "asano", deadline: "raigetumade" },
-  ];
+type Props ={
+  todoList: Todo[];
+}
+
+export const TodoList = ({todoList}: Props) => {
 
   return (
     <ul className="mt-4 ml-4 bg-indigo-500 p-4 rounded">
-      {todos.map((todo) => (
+      {todoList.map((todo) => (
         <TodoItem key={todo.id} task={todo.task} person={todo.person} deadline={todo.deadline} />
       ))}
     </ul>
