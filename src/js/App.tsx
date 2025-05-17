b4b9e7d614799649d709b48ae39014cd2e2e7862
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-17 15:38:41 +0900

    localstorageを使用してtodoListを記憶

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -8,24 +8,20 @@ import { Todo } from "./components/todo/type";
 export const App = () => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
 
-  const [count, setCount] = useState<number>(0);
+  useEffect(() => {
+    const todoListData = localStorage.getItem("todo-List");
+    if (todoListData) {
+    setTodoList(JSON.parse(todoListData));
+    }
+  }, []);
 
   useEffect(() => {
-    console.log(
-      "コンポーネントがマウントされた、もしくはtodoListが更新されました。",
-    );
-
-    return () => {
-      console.log("コンポーネントがアンマウントされました。");
-    };
-  }, []);
+    localStorage.setItem("todo-List", JSON.stringify(todoList));
+  },[todoList]);
 
   return (
     <main className="text-center mx-auto w-4/5">
       <Heading level="h1" classname='mb-10'>TODO</Heading>
-      <div>
-        カウント: {count} <button onClick={() => setCount(count + 1)}>ボタン</button>
-      </div>
       <Heading level="h2" classname="mb-8">新規TODO作成</Heading>
       <div className="mb-8">
         <NewTodoForm setTodoList={setTodoList}/>
