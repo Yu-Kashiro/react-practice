89d86c2660975ccb4604f618bc706c17126637c7
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-17 11:55:29 +0900

    useEffect実装

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,16 +1,31 @@
 import * as React from "react";
 import { TodoList } from "./components/todo/TodoList";
 import { Heading } from "./components/parts/Heading";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NewTodoForm } from "./components/todo/NewTodoForm";
 import { Todo } from "./components/todo/type";
 
 export const App = () => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
 
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    console.log(
+      "コンポーネントがマウントされた、もしくはtodoListが更新されました。",
+    );
+
+    return () => {
+      console.log("コンポーネントがアンマウントされました。");
+    };
+  }, []);
+
   return (
     <main className="text-center mx-auto w-4/5">
       <Heading level="h1" classname='mb-10'>TODO</Heading>
+      <div>
+        カウント: {count} <button onClick={() => setCount(count + 1)}>ボタン</button>
+      </div>
       <Heading level="h2" classname="mb-8">新規TODO作成</Heading>
       <div className="mb-8">
         <NewTodoForm setTodoList={setTodoList}/>
@@ -23,4 +38,3 @@ export const App = () => {
   );
 };
 export { Todo };
-
