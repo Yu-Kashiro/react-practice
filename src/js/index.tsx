4c99060172c011de67b19f0dbf0baae22eec6f43
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-21 10:04:13 +0900

    chakraUIを導入

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "../css/main.css";
 import { App } from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import { ChakraProvider } from '@chakra-ui/react'
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app")!);
 root.render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>,
+  <ChakraProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ChakraProvider>
 );
