d7be7966bbed1a31a251c2d543811689e4f88ea7
Yu-Kashiro <poyonnnnnn@gmail.com>
2025-05-24 19:43:34 +0900

    記事の詳細ページを追加

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -3,12 +3,14 @@ import { useAuth } from "./hooks/use-auth";
 import { Login } from "./pages/Login";
 import { Todo } from "./pages/Todo";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { TodoDetail } from "./pages/TodoDetail";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/login" element={<Login />} />
       <Route path="/todo" element={<Todo />} />
+      <Route path="/todo/:id" element={<TodoDetail />} />
     </>
   ),
 );
---
+++ src/js/pages/TodoDetail.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+export const TodoDetail = () => {
+  return <Box>記事の詳細ページです</Box>
+};
